7819271acd9626d3bfacb814b8737490bac1c6d6
paulcfx <paulcfx@gmail.com>
2021-11-03 20:49:53 -0400

    Create fetch.js

---
+++ node/fetch.js
@@ -0,0 +1,24 @@
+var details = {
+    'userName': 'test@gmail.com',
+    'password': 'Password!',
+    'grant_type': 'password'
+};
+
+var formBody = [];
+for (var property in details) {
+  var encodedKey = encodeURIComponent(property);
+  var encodedValue = encodeURIComponent(details[property]);
+  formBody.push(encodedKey + "=" + encodedValue);
+}
+formBody = formBody.join("&");
+
+fetch('https://example.com/login', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+  },
+  body: formBody
+})
+
+const formBody = Object.keys(details).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(details[key])).join('&');
+
